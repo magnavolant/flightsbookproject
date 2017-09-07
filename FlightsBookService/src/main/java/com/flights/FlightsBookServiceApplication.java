472d60ad7093379de46a574d05d522ec70e8510f
mskalbania <cod634.steam@gmail.com>
2017-09-07 20:10:53 +0200

    Initial push

---
+++ FlightsBookService/src/main/java/com/flights/FlightsBookServiceApplication.java
@@ -0,0 +1,12 @@
+package com.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightsBookServiceApplication.class, args);
+	}
+}
