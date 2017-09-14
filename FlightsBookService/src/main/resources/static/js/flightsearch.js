5d4d67fba978ff69a9c7f69d5bc04b05f14056d5
mskalbania <cod634.steam@gmail.com>
2017-09-14 19:27:55 +0200

    fixed login

--- FlightsBookService/src/main/resources/static/js/flightsearch.js
+++ FlightsBookService/src/main/resources/static/js/flightsearch.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
+    $("#search-btn").click(function (event) {
+        event.preventDefault();
+        //TODO NOW ITS SENDING INPUT SHOULD CONVERT TO AIRPORT CODE
+        var formData = {
+            whenceAirportCode: $("#flight-from").val(),
+            destinationAirportCode: $("#flight-to").val(),
+            departureDate: $("#flight-departing").val(),
+            peopleAmount: $("#adults-amount").val() +
+            $("#children-amount").val()
+        };
+        console.log(formData.whenceAirportCode + " " + formData.destinationAirportCode +
+        " " + formData.departureDate + " " + formData.peopleAmount);
+    });
 
-    $("#search-btn")
 
-    
-    
-    function sendData() {
-        
-    }
-    
 });
