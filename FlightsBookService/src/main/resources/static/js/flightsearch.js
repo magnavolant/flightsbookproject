c051522ab5019a02a2eff385df0ff24befbb56f4
mskalbania <cod634.steam@gmail.com>
2017-09-14 22:04:13 +0200

    added working ajax search scripts

--- FlightsBookService/src/main/resources/static/js/flightsearch.js
+++ FlightsBookService/src/main/resources/static/js/flightsearch.js
@@ -1,17 +1,37 @@
 $(document).ready(function () {
+
+
     $("#search-btn").click(function (event) {
         event.preventDefault();
         //TODO NOW ITS SENDING INPUT SHOULD CONVERT TO AIRPORT CODE
+
+        //Date picker widget is bugged so we are adding one day to each picked date
+        var date = $("#flight-departing").datepicker("getDate");
+        date.setDate(date.getDate() + 1);
+
         var formData = {
             whenceAirportCode: $("#flight-from").val(),
             destinationAirportCode: $("#flight-to").val(),
-            departureDate: $("#flight-departing").val(),
-            peopleAmount: $("#adults-amount").val() +
-            $("#children-amount").val()
+            departureDate: date.toISOString().slice(0,10),
+            peopleAmount: Number($("#adults-amount").find("option:selected").text()) +
+            Number($("#children-amount").find("option:selected").text())
         };
-        console.log(formData.whenceAirportCode + " " + formData.destinationAirportCode +
-        " " + formData.departureDate + " " + formData.peopleAmount);
+        console.log(JSON.stringify(formData));
+        postData(formData)
     });
 
-
+    function postData(data) {
+        $.ajax({
+            url: "/getflights",
+            type: "POST",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            success: function (data, status, jqXHR) {
+                console.log(JSON.stringify(data));
+                console.log(status);
+                console.log(jqXHR);
+                window.location.href = "http://stackoverflow.com"
+            }
+        });
+    }
 });
